71e65c8a92db42aa1103278cadbea5e0f23f3dc8
Alex Snyder <alystrife@gmail.com>
2021-10-23 21:07:08 -0400

    set API functional

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=6eb8aa1a8a913c3b40a8fdee22427620'
+
 let history = []; //empty array
 
 let baseapiurl = "https://api.openweathermap.org"
@@ -12,13 +14,65 @@ let list_group = document.querySelector("#list-group");
 
 function handleSearch(e) {
     e.preventDefault()
+    let city = input_search.value
     console.log("searchSubmitted")
-    fetch('https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=6eb8aa1a8a913c3b40a8fdee22427620')
+    fetch(`${baseapiurl}/data/2.5/weather?q=${city}&units=imperial&appid=${myApiKey}`)
         .then(response => response.json())
-        .then(data => console.log(data));
+        .then(data => {
+            let lat = data.coord.lat
+            let lon = data.coord.lon
+            fetch(`${baseapiurl}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=hourly,minutely&appid=${myApiKey}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                displayCurrentWeather(data.current);
+                displayForecast(data.daily);
+            });
+        });
 }
 function handleSearchHistory(e) {
     //tbd
+}
+
+function displayCurrentWeather(data) {
+    console.log("DISPLAY CURRENT WEATHER", data)
+    let date = new Date(data.dt * 1000)
+    let dateStr = date.toLocaleDateString("en-US")
+    let temperature = data.temp
+    let windSpeed = data.wind_speed
+    let humidity = data.humidity
+    let uv = data.uvi
+
+    console.log(dateStr, temperature, windSpeed, humidity, uv)
+
+    let container = document.getElementById("current")
+
+    let dateElement = document.createElement("p")
+    dateElement.innerHTML = dateStr
+
+    let temperatureElement = document.createElement("p")
+    temperatureElement.innerHTML = temperature + "F"
+
+    container.appendChild(dateElement)
+    container.appendChild(temperatureElement)
+}
+
+function displayForecast(data) {
+    console.log("DISPLAY FORECAST", data)
+
+
+
+    // let container = document.getElementById("")
+
+
+    // let someIcon = document.createElement("img")
+    // someIcon.setAttribute("src", "http://img.com/idk.jpg")
+
+    // let someText = document.createElement("p")
+    // someText.innerHTML = "hello"
+
+    // container.appendChild(someIcon)
+    // container.appendChild(someText)
 }
 
 search.addEventListener("click", handleSearch);
