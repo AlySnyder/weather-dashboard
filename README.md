8c0866df3c8cdaa3ca50806eb2d2e5c43e473522
Alex Snyder <alystrife@gmail.com>
2021-10-24 16:43:14 -0400

    fixed bootstrap and layout, added display forecast and search history

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# weather-dashboard
+### Welcome to a Weather Dashboard
+
+# Description
+
 A weather dashboard that will run in your browse,  and feature dynamically updated HTML and CSS.
+
+#
+
+
+#
+
+
+# The Github Repo
+
+# The URL 
+
--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=6eb8aa1a8a913c3b40a8fdee22427620'
 
-let history = []; //empty array
 
 let baseapiurl = "https://api.openweathermap.org"
 let myApiKey = "6eb8aa1a8a913c3b40a8fdee22427620"
@@ -11,75 +10,156 @@ let five_day_forcast = document.querySelector("#five-day-forecast");
 let list_group = document.querySelector("#list-group");
 
 
-
-function handleSearch(e) {
+function searchClicked(e) {
     e.preventDefault()
     let city = input_search.value
+    addSearchHistory(city)
     console.log("searchSubmitted")
+    performSearch(city)
+}
+
+function performSearch(city) {
     fetch(`${baseapiurl}/data/2.5/weather?q=${city}&units=imperial&appid=${myApiKey}`)
         .then(response => response.json())
         .then(data => {
             let lat = data.coord.lat
             let lon = data.coord.lon
             fetch(`${baseapiurl}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=hourly,minutely&appid=${myApiKey}`)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data)
-                displayCurrentWeather(data.current);
-                displayForecast(data.daily);
-            });
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    displayCurrentWeather(data.current);
+                    displayForecast(data.daily);
+                });
         });
 }
-function handleSearchHistory(e) {
-    //tbd
+
+function addSearchHistory(value) {
+    var history = JSON.parse(localStorage.getItem("weatherHistory"));
+    if (!history) {
+        history = []
+    }
+
+    history.push(value);
+    localStorage.setItem("weatherHistory", JSON.stringify(history));
+    loadSearchHistory()
 }
+
+function loadSearchHistory() {
+    let searchHistory = document.getElementById("search-history")
+    searchHistory.innerHTML = ""
+
+    var history = JSON.parse(localStorage.getItem("weatherHistory"))
+    for (var i = 0; i < history.length; i++) {
+        let city = history[i]
+
+        let row = document.createElement("div")
+        row.classList = "row mx-auto mt-2"
+
+        let button = document.createElement("button");
+        button.type = "button"
+        button.classList = "btn btn-secondary"
+        button.innerHTML = history[i]
+
+        button.onclick = function() {
+            console.log(city)
+            input_search.value = city
+            performSearch(city)
+        }
+
+        row.appendChild(button)
+        searchHistory.appendChild(row)
+    }
+}
+
 
 function displayCurrentWeather(data) {
     console.log("DISPLAY CURRENT WEATHER", data)
     let date = new Date(data.dt * 1000)
     let dateStr = date.toLocaleDateString("en-US")
-    let temperature = data.temp
-    let windSpeed = data.wind_speed
-    let humidity = data.humidity
-    let uv = data.uvi
 
-    console.log(dateStr, temperature, windSpeed, humidity, uv)
+    let titleText = document.getElementById("current-title");
+    titleText.innerHTML = `${input_search.value} (${dateStr})`
 
-    let container = document.getElementById("current")
+    let iconImg = document.getElementById("current-icon");
+    iconImg.setAttribute("src", `http://openweathermap.org/img/w/${data.weather[0].icon}.png`)
 
-    let dateElement = document.createElement("p")
-    dateElement.innerHTML = dateStr
+    let tempText = document.getElementById("current-temp");
+    tempText.innerHTML = `Temp: ${data.temp}°F`
 
-    let temperatureElement = document.createElement("p")
-    temperatureElement.innerHTML = temperature + "F"
+    let windText = document.getElementById("current-wind");
+    windText.innerHTML = `Wind: ${data.wind_speed} mph`
 
-    container.appendChild(dateElement)
-    container.appendChild(temperatureElement)
+    let humidityText = document.getElementById("current-humidity");
+    humidityText.innerHTML = `Humidity: ${data.humidity}%`
+
+    let uviText = document.getElementById("current-uvi");
+    uviText.innerHTML = `UV Index: `
+
+    uviColor = "bg-success"
+    if (data.uvi >= 3) {
+        uviColor = "bg-warning"
+    }
+    if (data.uvi >= 6) {
+        uviColor = "bg-danger"
+    }
+
+    let uviBadge = document.createElement("span")
+    uviBadge.classList = `badge rounded-pill ${uviColor}`
+    uviBadge.innerHTML = data.uvi
+    uviText.appendChild(uviBadge)
 }
+
 
 function displayForecast(data) {
     console.log("DISPLAY FORECAST", data)
 
+    let forecastContainer = document.getElementById("forecast");
+    forecastContainer.innerHTML = ""
 
+    for (let i = 1; i <= 5; i++) {
+        let card = document.createElement("div");
+        card.classList = "card text-white bg-dark mb-3 me-2";
+        card.style = "max-width: 12rem;"
 
-    // let container = document.getElementById("")
+        let date = new Date(data[i].dt * 1000)
+        let dateStr = date.toLocaleDateString("en-US")
 
+        let cardHeader = document.createElement("div")
+        cardHeader.classList = "card-header"
+        cardHeader.innerHTML = dateStr
 
-    // let someIcon = document.createElement("img")
-    // someIcon.setAttribute("src", "http://img.com/idk.jpg")
+        let cardBody = document.createElement("div")
+        cardBody.classList = "card-body"
 
-    // let someText = document.createElement("p")
-    // someText.innerHTML = "hello"
+        let iconImg = document.createElement("img")
+        iconImg.setAttribute("src", `http://openweathermap.org/img/w/${data[i].weather[0].icon}.png`)
+        cardBody.appendChild(iconImg)
 
-    // container.appendChild(someIcon)
-    // container.appendChild(someText)
+        let tempText = document.createElement("p")
+        tempText.classList = "card-text"
+        tempText.innerHTML = `Temp: ${data[i].temp.day}°F`
+        cardBody.appendChild(tempText)
+
+        let windText = document.createElement("p")
+        windText.classList = "card-text"
+        windText.innerHTML = `Wind: ${data[i].wind_speed} mph`
+        cardBody.appendChild(windText)
+
+        let humidityText = document.createElement("p")
+        humidityText.classList = "card-text"
+        humidityText.innerHTML = `Humidity: ${data[i].humidity}%`
+        cardBody.appendChild(humidityText)
+
+        card.appendChild(cardHeader)
+        card.appendChild(cardBody)
+
+        forecastContainer.appendChild(card)
+    }
 }
 
-search.addEventListener("click", handleSearch);
+
+search.addEventListener("click", searchClicked);
 console.log("listenerForSearchButtonAdded")
-list_group.addEventListener("click", handleSearchHistory);
-console.log("listenerForListGroupAdded-1")
-console.log("listenerForListGroupAdded-2")
-console.log("listenerForListGroupAdded-3")
-console.log("listenerForListGroupAdded-4")
-console.log(Search)
+
+loadSearchHistory()
